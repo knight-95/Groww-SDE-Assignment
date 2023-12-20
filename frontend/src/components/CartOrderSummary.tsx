3567c1d52ab167d57d48a793e75a555e3e9243e0
Yash Gupta <yashgupta13.in@gmail.com>
2023-12-20 11:52:44 +0530

    updated cart total function

--- frontend/src/components/CartOrderSummary.tsx
+++ frontend/src/components/CartOrderSummary.tsx
@@ -2,61 +2,108 @@ import {
   Button,
   Flex,
   Heading,
+  Input,
   Link,
   Stack,
   Text,
   useColorModeValue as mode,
-} from '@chakra-ui/react'
-import { FaArrowRight } from 'react-icons/fa'
-import { formatPrice } from './PriceTag'
+} from "@chakra-ui/react";
+import { FaArrowRight } from "react-icons/fa";
+import { formatPrice } from "./PriceTag";
+import { useState } from "react";
 
 type OrderSummaryItemProps = {
-  label: string
-  value?: string
-  children?: React.ReactNode
-}
+  label: string;
+  value?: string;
+  children?: React.ReactNode;
+  totalValue?: any;
+};
 
 const OrderSummaryItem = (props: OrderSummaryItemProps) => {
-  const { label, value, children } = props
+  const { label, value, children } = props;
   return (
     <Flex justify="space-between" fontSize="sm">
-      <Text fontWeight="medium" color={mode('gray.600', 'gray.400')}>
+      <Text fontWeight="medium" color={mode("gray.600", "gray.400")}>
         {label}
       </Text>
       {value ? <Text fontWeight="medium">{value}</Text> : children}
     </Flex>
-  )
-}
+  );
+};
 
-export const CartOrderSummary = () => {
+export const CartOrderSummary = ({ totalValue }: any) => {
+  const [couponCode, setCouponCode] = useState<string>("");
+  const [isCouponCodeVisible, setIsCouponCodeVisible] =
+    useState<boolean>(false);
+  const applyCoupon = (code: string) => {
+    // TODO: Implement coupon validation logic here
+    // For simplicity, let's assume the coupon code is valid and provides a 10% discount
+    const discountPercentage = 10;
+    const discountedTotal = totalValue * ((100 - discountPercentage) / 100);
+    // Update the total value with the discounted amount
+    // setTotalValue(discountedTotal);
+  };
+
+  const handleShowCouponCode = () => {
+    setIsCouponCodeVisible(!isCouponCodeVisible);
+  };
+
   return (
     <Stack spacing="8" borderWidth="1px" rounded="lg" padding="8" width="full">
       <Heading size="md">Order Summary</Heading>
 
       <Stack spacing="6">
-        <OrderSummaryItem label="Subtotal" value={formatPrice(597)} />
+        <OrderSummaryItem label="Subtotal" value={formatPrice(totalValue)} />
         <OrderSummaryItem label="Shipping + Tax">
           <Link href="#" textDecor="underline">
             Calculate shipping
           </Link>
         </OrderSummaryItem>
         <OrderSummaryItem label="Coupon Code">
-          <Link href="#" textDecor="underline">
+          <Link onClick={handleShowCouponCode} textDecor="underline">
             Add coupon code
           </Link>
         </OrderSummaryItem>
+        {isCouponCodeVisible && (
+          <Stack
+            spacing="8"
+            borderWidth="1px"
+            rounded="lg"
+            padding="8"
+            width="full"
+          >
+            <OrderSummaryItem label="Coupon Code">
+              <Flex>
+                <Input
+                  type="text"
+                  placeholder="Enter coupon code"
+                  value={couponCode}
+                  onChange={(e) => setCouponCode(e.target.value)}
+                />
+                <Button ml="2" onClick={() => applyCoupon(couponCode)}>
+                  Apply
+                </Button>
+              </Flex>
+            </OrderSummaryItem>
+          </Stack>
+        )}
         <Flex justify="space-between">
           <Text fontSize="lg" fontWeight="semibold">
             Total
           </Text>
           <Text fontSize="xl" fontWeight="extrabold">
-            {formatPrice(597)}
+            {formatPrice(totalValue)}
           </Text>
         </Flex>
       </Stack>
-      <Button colorScheme="blue" size="lg" fontSize="md" rightIcon={<FaArrowRight />}>
+      <Button
+        colorScheme="blue"
+        size="lg"
+        fontSize="md"
+        rightIcon={<FaArrowRight />}
+      >
         Checkout
       </Button>
     </Stack>
-  )
-}
+  );
+};
--- frontend/src/components/CartPage.tsx
+++ frontend/src/components/CartPage.tsx
@@ -20,7 +20,17 @@ const CartPage = () => {
     products: [],
     paymentMethods: [],
   };
-  
+
+  // Function to calculate the total value based on products in the cart
+  const calculateTotalValue = () => {
+    return products.reduce(
+      (total, product) => total + product.price * product.quantity,
+      0
+    );
+  };
+
+  const totalValue = calculateTotalValue();
+
   // Handling loading and error states
   if (loading) {
     return <div>Loading...</div>;
@@ -45,27 +55,19 @@ const CartPage = () => {
         >
           <Stack spacing={{ base: "8", md: "10" }} flex="2">
             <Heading fontSize="2xl" fontWeight="extrabold">
-              Shopping Cart (3 items)
+              Shopping Cart ({products.length}{" "}
+              {products.length > 1 ? "Items" : "Item"})
             </Heading>
 
             <Stack spacing="6">
               {products.map((product) => (
                 <CartItem key={product.id} {...product} />
               ))}
-
-              {products.map((product) => (
-                <div key={product.id}>
-                  <p>{product.title}</p>
-                  <p>Quantity: {product.quantity}</p>
-                  <p>Price: ₹{product.price}</p>
-                  {/* <Image src={product.image} alt={product.title} /> */}
-                </div>
-              ))}
             </Stack>
           </Stack>
 
           <Flex direction="column" align="center" flex="1">
-            <CartOrderSummary />
+            <CartOrderSummary totalValue={totalValue} />
             <HStack mt="6" fontWeight="semibold">
               <p>or</p>
               <Link color={mode("blue.500", "blue.200")}>
