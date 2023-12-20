cd358d250a7f58f734c18132543f6a2412e32fba
Yash Gupta <yashgupta13.in@gmail.com>
2023-12-20 12:18:34 +0530

    updated max quantity to be 20

--- frontend/src/components/QuantitySelect.tsx
+++ frontend/src/components/QuantitySelect.tsx
@@ -10,7 +10,7 @@ const QuantitySelect = ({ defaultQuantity = 1, onChangeQuantity }: QuantitySelec
   const [quantity, setQuantity] = useState(defaultQuantity);
 
   const handleIncrease = () => {
-    const newQuantity = quantity + 1;
+    const newQuantity = Math.min(quantity + 1, 20); // Limit to a maximum of 20
     setQuantity(newQuantity);
     if (onChangeQuantity) {
       onChangeQuantity(newQuantity);
@@ -18,7 +18,7 @@ const QuantitySelect = ({ defaultQuantity = 1, onChangeQuantity }: QuantitySelec
   };
 
   const handleDecrease = () => {
-    const newQuantity = Math.max(quantity - 1, 1);
+    const newQuantity = Math.max(quantity - 1, 1); // Limit to a minimum of 1
     setQuantity(newQuantity);
     if (onChangeQuantity) {
       onChangeQuantity(newQuantity);
@@ -31,7 +31,7 @@ const QuantitySelect = ({ defaultQuantity = 1, onChangeQuantity }: QuantitySelec
         -
       </Button>
       <div>{quantity}</div>
-      <Button onClick={handleIncrease} variant="outline">
+      <Button onClick={handleIncrease}  variant="outline">
         +
       </Button>
     </HStack>
